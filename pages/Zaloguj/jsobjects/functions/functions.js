7307820874aad42047fc35baa30679c9dce1c624
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-31 19:35:34 +0000

    31-10-20-35

--- pages/Zaloguj/jsobjects/functions/functions.js
+++ pages/Zaloguj/jsobjects/functions/functions.js
@@ -10,33 +10,52 @@ export default {
 		return dcodeIO.bcrypt.hashSync(inp_registerPassword.text, 10);
 	},
 
+	//verifyHash: async (password, hash) => {
+	//	return dcodeIO.bcrypt.compareSync(password, hash)
+	//},
+	
 	verifyHash: async (password, hash) => {
-		return dcodeIO.bcrypt.compareSync(password, hash)
-	},
+return await dcodeIO.bcrypt.compare(password, hash);
+},
+
 
 	createToken: async (user) => {
 		return jsonwebtoken.sign(user, 'secret', {expiresIn: 60*60});
 	},
 
-	signIn: async () => {
-		const password = inp_password.text;
+//	signIn: async () => {
+//		const password = inp_password.text;
 
-		const [user] = await findUserByEmail.run();
+//		const [user] = await findUserByEmail.run();
 
-		if (user && this.verifyHash(password, user?.password_hash)) {
+//		if (user && this.verifyHash(password, user?.password_hash)) {
 		
-			storeValue('token', await this.createToken(user))
-			storeValue('uzytkownik',inp_email.text )
+//			storeValue('token', await this.createToken(user))
+//			storeValue('uzytkownik',inp_email.text )
 		//		.then(() => updateLogin.run({
 	//			id_user: user.id
 	//		}))
-				.then(() => showAlert('zalogowano', 'success'))
-				.then(() => navigateTo('Klienci', {}, 'SAME_WINDOW'))	; 	
+//				.then(() => showAlert('zalogowano', 'success'))
+				//.then(() => navigateTo('Klienci', {}, 'SAME_WINDOW'))	; 	
 			  
-		} else {
-			return showAlert('Invalid emaill/password combination', 'error');
-		}
-	},
+//		} else {
+//			return showAlert('Invalid emaill/password combination', 'error');
+//		}
+//	},
+	
+	signIn: async () => {
+const password = inp_password.text;
+const [user] = await findUserByEmail.run();
+
+if (user && await this.verifyHash(password, user?.password_hash)) {
+await storeValue('token', await this.createToken(user));
+await updateLogin.run({ id: user.id });
+showAlert('Zalogowano', 'success');
+navigateTo('Klienci', {}, 'SAME_WINDOW');
+} else {
+showAlert('Błędny mail lub hasło', 'error');
+}
+},
 
 	register: async () => {
 		const passwordHash = await this.generatePasswordHash();
